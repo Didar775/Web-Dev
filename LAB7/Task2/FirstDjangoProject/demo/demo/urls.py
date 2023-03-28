0296850a972ba877f68a6653873389bc408e48b9
didar775 <d_alikhan@kbtu.kz>
2023-03-29 02:39:30 +0600

    main

--- LAB7/Task2/FirstDjangoProject/demo/demo/urls.py
+++ LAB7/Task2/FirstDjangoProject/demo/demo/urls.py
@@ -1,4 +1,4 @@
-"""FirstDjangoApp URL Configuration
+"""demo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
